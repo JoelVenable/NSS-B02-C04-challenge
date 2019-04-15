95fbfe7cda2fde11ba0305ed1e8148fbb50c08ca
Joel Venable <joelmusicman@gmail.com>
2019-04-15 14:30:03 -0500

    The tree can be rendered to HTML

--- js/main.js
+++ js/main.js
@@ -1,18 +1,48 @@
+const headerFrag = document.createDocumentFragment(),
+  container = document.querySelector("#container");
 
-function towerBuilder(n) { // n = number of floors
-  var out = Array(n).fill('');
-  const t = (n * 2) - 1, // total characters per row
-    m = n - 1; //midpoint
-  function isDot(i, j) {
-    if (j < m - i || j > m + i) return " ";
-    else return "*";
+function buildForm() {
+  const fragment = document.createDocumentFragment(),
+    div = createHtmlElement('div','',fragment,['formDiv']),
+    form = createHtmlElement('form','',div, );
+  form.setAttribute('method', 'post');
+  form.setAttribute('action', 'javascript:buildTree()');
+  addInputItem("text", "height", "Height of christmas tree: ", form);
+  addInputItem("text", "char", "Character: ", form);
+  addInputItem("submit", "submit", "Make a tree!", form);
+  document.querySelector('#container').appendChild(fragment);
+}
+
+
+
+
+buildForm();
+
+
+
+function createHtmlElement(tag, content, parent, classList) {
+  const element = document.createElement(tag);
+  element.textContent = content;
+  if (classList !== undefined) {
+    element.classList.add(...classList);
   }
+  parent.appendChild(element);
+  return element;
+}
 
 
-  for (let i = 0; i < n; i++) { // floor number
-    for (let j = 0; j < t; j++) { // lateral position
-      out[i] += isDot(i, j);
-    }
-  }
-  return out;
+
+
+function addInputItem(type, id, text, parent) {
+  let item = document.createElement('input');
+  item.setAttribute("type", type);
+  item.id = id;
+  let label = document.createElement('label');
+  label.setAttribute('for', id);
+
+  label.textContent = text;
+  parent.appendChild(label);
+  parent.appendChild(item);
+
+
 }
