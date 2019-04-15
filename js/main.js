53821d9ce5f51f7f350a30a57075c4fa2849a8a3
Joel Venable <joelmusicman@gmail.com>
2019-04-15 08:57:11 -0500

    Imported old work from codewars

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+function towerBuilder(n) { // n = number of floors
+  var out = Array(n).fill('');
+  const t = (n * 2) - 1, // total characters per row
+    m = n - 1; //midpoint
+  function isDot(i, j) {
+    if (j < m - i || j > m + i) return " ";
+    else return "*";
+  }
+
+
+  for (let i = 0; i < n; i++) { // floor number
+    for (let j = 0; j < t; j++) { // lateral position
+      out[i] += isDot(i, j);
+    }
+  }
+  return out;
+}
